4d7a767f277121965fb6fea66603f547c0cda6b9
Javi <javisr997@gmail.com>
2021-01-28 18:29:57 +0100

    Laravel

---
+++ Laravel/example-app/resources/views/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Índice</title>
+    </head>
+    <body>
+        <h1>Hola mundo</h1>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit,
+            animi natus, exercitationem quaerat in nobis iure architecto
+            necessitatibus nulla, dolore cum et assumenda voluptates ex nam
+            repellat! Voluptatem voluptas, laborum impedit vitae nesciunt omnis
+            explicabo? Quam quos facere quod. Rem atque vero deleniti aliquam
+            laboriosam eum amet perferendis? Autem, voluptatibus.
+        </p>
+    </body>
+</html>
